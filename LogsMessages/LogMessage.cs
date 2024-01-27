2e0d477a9aa21a3f5fea4d71202e963721a07cef
Flutes <Flutes@DESKTOP-9I523O8>
2024-01-27 17:50:23 +0100

    temporary fix (dockignore and logs folder)

---
+++ LogsMessages/LogMessage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noexia.MyAI.Inferences.Core.Logs
+{
+    public struct LogMessage
+    {
+        public string Title { get; private set; }
+        public string Message { get; private set; }
+        public ELogMessage Type { get; private set; }
+
+        public LogMessage(string title, string message, ELogMessage type)
+        {
+            Title = title;
+            Message = message;
+            Type = type;
+        }
+
+        public LogMessage(string title, string message) : this(title, message, ELogMessage.Default)
+        {
+        }
+
+        public LogMessage(string message, ELogMessage type) : this(string.Empty, message, type)
+        {
+        }
+
+        public LogMessage(string message) : this(string.Empty, message, ELogMessage.Default)
+        {
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(Title))
+            {
+                return string.Format("{0} : {1}", Type.ToString(), Message);
+            }
+            else
+            {
+                return string.Format("{0} : {1}", Title, Message);
+            }
+        }
+    }
+}
---
+++ LogsMessages/LogsManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noexia.MyAI.Inferences.Core.Logs
+{
+    public static class LogsManager
+    {
+        public static event Action<LogMessage> OnLogMessage;
+
+        public static void Log(string title, string message, ELogMessage type)
+        {
+            OnLogMessage?.Invoke(new LogMessage(title, message, type));
+        }
+
+        public static void Log(string title, string message)
+        {
+            OnLogMessage?.Invoke(new LogMessage(title, message));
+        }
+
+        public static void Log(string message, ELogMessage type)
+        {
+            OnLogMessage?.Invoke(new LogMessage(message, type));
+        }
+
+        public static void Log(string message)
+        {
+            OnLogMessage?.Invoke(new LogMessage(message));
+        }
+
+        public static void LogWarning(string title, string message)
+        {
+            OnLogMessage?.Invoke(new LogMessage(title, message, ELogMessage.Warning));
+        }
+
+        public static void LogWarning(string message)
+        {
+            OnLogMessage?.Invoke(new LogMessage(message, ELogMessage.Warning));
+        }
+
+        public static void LogError(string title, string message)
+        {
+            OnLogMessage?.Invoke(new LogMessage(title, message, ELogMessage.Error));
+        }
+
+        public static void LogError(string message)
+        {
+            OnLogMessage?.Invoke(new LogMessage(message, ELogMessage.Error));
+        }
+
+        public static void RegisterToConsole()
+        {
+            OnLogMessage += (log) =>
+            {
+                Console.WriteLine(log.ToString());
+            };
+        }
+
+        public static void RegisterToDebug()
+        {
+            OnLogMessage += (log) =>
+            {
+                System.Diagnostics.Debug.WriteLine(log.ToString());
+            };
+        }
+    }
+}
